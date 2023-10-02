3843544149514e16ca65c3647ae926281c7aea0f
Daniel Adigun <105540876+Russoboy@users.noreply.github.com>
2023-10-02 14:47:26 +0100

    Update Weather-app.js
    Edited!

--- Weather-app.js
+++ Weather-app.js
@@ -8,3 +8,41 @@ const  weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
       const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
      
+  
+      if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+      }else{
+        var data = await response.json();
+      document.querySelector(".city").innerHTML = data.name;
+      document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + " c";
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+    
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = ""
+    }
+    elseif(data.weather[0].main == "Clear")
+     {
+        weatherIcon.src = ""
+    } 
+    elseif(data.weather[0].main == "Rain") 
+    {
+        weatherIcon.src = ""
+    }
+    elseif(data.weather[0].main == "Drizzle") 
+    {
+        weatherIcon.src = ""
+    }
+    elseif(data.weather[0].main == "Mist") 
+    {
+        weatherIcon.src = ""
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+      }
+
+      console.log(data) 
+     
+    }
