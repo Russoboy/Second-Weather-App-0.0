606ec09d11711953d4f557fda9216197fe677e1c
Daniel Adigun <105540876+Russoboy@users.noreply.github.com>
2023-10-06 21:11:42 +0100

    Update Weather-app.js
    Done

--- Weather-app.js
+++ Weather-app.js
@@ -1,5 +1,7 @@
-const apiKey = "cd91157f12ce9e57fd64dc3a66e598e6";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+//Input your apiKey below
+const apiKey = "";
+//Input your apiUrl below
+const apiUrl = "";
 
 const  searchBox = document.querySelector(".search input");
 const  searchBtn = document.querySelector(".search button");
